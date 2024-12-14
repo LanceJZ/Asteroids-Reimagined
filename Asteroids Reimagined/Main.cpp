d9aa53eedbbbe3e2c541628d676252c727da8543
Purple Knightmare <lance.zimmerman@gmail.com>
2024-12-14 09:55:30 -0800

    Fixed Output folder for raylib on release, so game gets compiled using current raylib version.

--- Asteroids Reimagined/Main.cpp
+++ Asteroids Reimagined/Main.cpp
@@ -33,7 +33,7 @@ int WinMain()
 	int windowHeight = 960; //height
 	int windowWidth = 1280; //width
 
-	InitWindow(windowWidth, windowHeight, "Asteroids Reimagined - RC 4.54.434");
+	InitWindow(windowWidth, windowHeight, "Asteroids Reimagined - RC 4.54.435");
 	InitAudioDevice();
 
 	Image icon = LoadImage("icon.png");
